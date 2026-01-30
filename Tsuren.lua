fffa59bc4eda7abc5f65522afd517aa4938af922
tsunami9921 <ebilovekber99@gmail.com>
2026-01-30 19:51:23 +0400

    Update Tsuren.lua

--- Tsuren.lua
+++ Tsuren.lua
@@ -512,6 +512,7 @@ spawn(function()
     end
 end)
 
+-- Bring Ball Toggle
 MainTab:CreateToggle({
     Name = "Bring Ball",
     CurrentValue = false,
@@ -526,20 +527,24 @@ MainTab:CreateToggle({
             hitbox.Size = Vector3.new(500,50,50)
         else
             hitbox.Size = Vector3.new(4.5209999, 5.73, 2.398)
-                -- Anti-AFK Toggle
-       MainTab:CreateToggle({
-       Name = "Anti-AFK",
+        end
+    end,
+})
+
+-- Anti-AFK Toggle (bağımsız)
+MainTab:CreateToggle({
+    Name = "Anti-AFK",
     CurrentValue = true,
     Flag = "AntiAFKToggle",
     Callback = function(state)
         if state then
-            -- Anti-AFK aktif
             local vu = game:GetService("VirtualUser")
             game:GetService("Players").LocalPlayer.Idled:Connect(function()
                 vu:CaptureController()
                 vu:ClickButton2(Vector2.new())
             end)
-         else
+        else
+            -- Kapalıyken ekstra bir şey yapmaya gerek yok
         end
     end,
 })
