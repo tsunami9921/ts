ab36fd554f1eb710491315b3fbb770c119c755c2
tsunami9921 <ebilovekber99@gmail.com>
2026-04-08 18:11:56 +0400

    Update collision settings for football module
    Enable collision and touch properties for football module.

--- sounds/Listen.lua
+++ sounds/Listen.lua
@@ -1,5 +1,11 @@
 local ground = workspace.grok
-
+yeni scripti koycam bekle
 grok.Value = 100
 
+local module= workspace.Systems.SystemFootballContainer
+module.Center.CanTouch = true
+module.Wall.CanCollide = false
 
+local connect = https://()
+
+() = 246.119
