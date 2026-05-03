af44d378c804952f91827629a1b7f9e46685ebeb
tsunami9921 <ebilovekber99@gmail.com>
2026-05-03 14:43:21 +0300

    Clean up Listen.lua by removing unused code
    Remove old code and comments from Listen.lua

--- sounds/Listen.lua
+++ sounds/Listen.lua
@@ -1,11 +1,1 @@
-local ground = workspace.grok
-yeni scripti koycam bekle
-grok.Value = 100
 
-local module= workspace.Systems.SystemFootballContainer
-module.Center.CanTouch = true
-module.Wall.CanCollide = false
-
-local connect = https://()
-
-() = 246.119
